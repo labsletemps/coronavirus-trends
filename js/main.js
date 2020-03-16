f3d023903c5bb66fbe6363f3db2925c3c0fbe70e
Paul Ronga <paul.ronga@laposte.net>
2020-03-16 16:02:56 +0100

    add template

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*
+
+Note de Paul:
+Dans ce fichier, je peux créer une scène ImageMagick pour déclencher des effets de scroll.
+Et on peut concaténer tous les scripts (avec webpack / CodeKit etc.), je peux le faire moi quand on passe en prod
+
+*/
