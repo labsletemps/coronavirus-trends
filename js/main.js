cc8b534d37bcefec8443dad7e4cfd89e70b91695
Jay <jeremie.rappaz@epfl.ch>
2020-03-17 15:09:22 +0100

    multi-coutries plot

--- js/main.js
+++ js/main.js
@@ -5,3 +5,24 @@ Dans ce fichier, je peux créer une scène ImageMagick pour déclencher des effe
 Et on peut concaténer tous les scripts (avec webpack / CodeKit etc.), je peux le faire moi quand on passe en prod
 
 */
+
+
+$( document ).ready(function() {
+
+    trend_graph("CH");
+
+    d3.select("#country-option").on("change", change)
+    function change() {
+      $("#chartTop").empty();
+      console.log(this.selectedIndex);
+      if (this.selectedIndex == 0){
+        trend_graph("CH");
+      } else if (this.selectedIndex == 1){
+        trend_graph("IT");
+      } else if (this.selectedIndex == 2){
+        trend_graph("DE");
+      } else if (this.selectedIndex == 3){
+        trend_graph("FR");
+      }
+    }
+});
