0a0ab44e6c97af6745f743536b3e27a2efa8f5e9
francoisquellec <francois.quellec@gmail.com>
2020-03-17 15:38:04 +0100

    static map done, TODO change color scales and dataset

--- js/europeMap.js
+++ js/europeMap.js
@@ -9,18 +9,21 @@ var projection = d3.geoMercator()
     .scale(700)                       // This is like the zoom
     .translate([ width/2, height/2 ])
 
-Promise.all([d3.json("/data/europe.geojson"), d3.csv("/data/geo_tweets.csv")]).then(function(data) {
-  dataGeo = data[0]
-  data = data[1]
+// Data and color scale
+var colorScale = d3.scaleLinear().domain([0,20000])
+  .range(["#b8b8b8", "red"]);
 
-  // Create a color scale
-  var allContinent = d3.map(data, function(d){return(d.tweets)}).keys()
-  var color = d3.scaleOrdinal()
-    .domain(allContinent)
-    .range(d3.schemePaired);
+Promise.all([d3.json("/data/world_countries.json"), d3.csv("/data/geo_tweets.csv")]).then(function(data) {
+  var dataGeo = data[0];
+  var dataTweets = data[1];
+  var dataMap = {};
+  dataTweets.forEach(function(d){
+    dataMap[d.iso_code] = d.tweets;
+  });
+  var dataMap = d3.map(dataMap)
 
   // Add a scale for bubble size
-  var valueExtent = d3.extent(data, function(d) { return +d.tweets; })
+  var valueExtent = d3.extent(dataTweets, function(d) { return +d.tweets; })
   var size = d3.scaleSqrt()
     .domain(valueExtent)  // What's in the data
     .range([ 1, 10])  // Size in pixel
@@ -31,23 +34,31 @@ Promise.all([d3.json("/data/europe.geojson"), d3.csv("/data/geo_tweets.csv")]).t
       .data(dataGeo.features)
       .enter()
       .append("path")
-        .attr("fill", "#b8b8b8")
+        .attr("fill", function (d) {
+        d.total = dataMap.get(d.id) || 0;
+        return colorScale(d.total);
+        })//, "#b8b8b8")
         .attr("d", d3.geoPath()
             .projection(projection)
         )
-      .style("stroke", "none")
+      .style("stroke", "None")
+        .on('mouseover', function(d) {
+          d3.select(this).style('stroke', 'black');
+        }).on('mouseout', function(d) {
+          d3.select(this).style('stroke', 'None');
+        })
       .style("opacity", .3)
 
   // Add circles:
   svg
     .selectAll("myCircles")
-    .data(data.sort(function(a,b) { return +b.tweets - +a.tweets }).filter(function(d,i){ return i<1000 }))
+    .data(dataTweets.sort(function(a,b) { return +b.tweets - +a.tweets }).filter(function(d,i){ return i<1000 }))
     .enter()
     .append("circle")
       .attr("cx", function(d){ return projection([+d.lon, +d.lat])[0] })
       .attr("cy", function(d){ return projection([+d.lon, +d.lat])[1] })
       .attr("r", function(d){ return size(+d.tweets) })
-      .style("fill", function(d){ return color(d.tweets) })
+      .style("fill", function(d){ return colorScale(d.tweets) })
       .attr("stroke", function(d){ if(d.tweets>20000){return "black"}else{return "none"}  })
       .attr("stroke-width", 1)
       .attr("fill-opacity", .4)
@@ -65,49 +76,4 @@ Promise.all([d3.json("/data/europe.geojson"), d3.csv("/data/geo_tweets.csv")]).t
       //.html("WHERE SURFERS LIVE")
       .style("font-size", 14)
 
-
-  // --------------- //
-  // ADD LEGEND //
-  // --------------- //
-  /*
-  // Add legend: circles
-  var valuesToShow = [100,4000,15000]
-  var xCircle = 40
-  var xLabel = 90
-  svg
-    .selectAll("legend")
-    .data(valuesToShow)
-    .enter()
-    .append("circle")
-      .attr("cx", xCircle)
-      .attr("cy", function(d){ return height - size(d) } )
-      .attr("r", function(d){ return size(d) })
-      .style("fill", "none")
-      .attr("stroke", "black")
-
-  // Add legend: segments
-  svg
-    .selectAll("legend")
-    .data(valuesToShow)
-    .enter()
-    .append("line")
-      .attr('x1', function(d){ return xCircle + size(d) } )
-      .attr('x2', xLabel)
-      .attr('y1', function(d){ return height - size(d) } )
-      .attr('y2', function(d){ return height - size(d) } )
-      .attr('stroke', 'black')
-      .style('stroke-dasharray', ('2,2'))
-
-  // Add legend: labels
-  svg
-    .selectAll("legend")
-    .data(valuesToShow)
-    .enter()
-    .append("text")
-      .attr('x', xLabel)
-      .attr('y', function(d){ return height - size(d) } )
-      .text( function(d){ return d } )
-      .style("font-size", 10)
-      .attr('alignment-baseline', 'middle')
-  */
 });
